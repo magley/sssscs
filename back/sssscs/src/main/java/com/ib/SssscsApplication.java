810235c5e32d5d87196ca7a1ea731ec82b96ce38
magley <magazinlazar2@gmail.com>
2023-03-14 15:37:59 +0100

    chore: Add empty spring project for backend

---
+++ back/sssscs/src/main/java/com/ib/SssscsApplication.java
@@ -0,0 +1,13 @@
+package com.ib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SssscsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SssscsApplication.class, args);
+	}
+
+}
