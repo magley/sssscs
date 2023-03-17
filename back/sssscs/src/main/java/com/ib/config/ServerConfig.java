5ab991d68fb8ebc74af6f492dbb44ba0961d52ba
magley <60552482+magley@users.noreply.github.com>
2023-03-17 20:56:16 +0100

    refactor: Simplify permissive CORS mapping

--- back/sssscs/src/main/java/com/ib/config/ServerConfig.java
+++ back/sssscs/src/main/java/com/ib/config/ServerConfig.java
@@ -13,7 +13,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class ServerConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
+        registry.addMapping("/**").allowedMethods("*");
     }
 
     @Bean
