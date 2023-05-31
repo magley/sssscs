595d1d2d7e383f857d6a71322557699b24a54aa3
magley <magazinlazar2@gmail.com>
2023-05-31 20:11:12 +0200

    feat: Add server-side validation

--- back/sssscs/src/main/java/com/ib/user/dto/PasswordRotationDto.java
+++ back/sssscs/src/main/java/com/ib/user/dto/PasswordRotationDto.java
@@ -1,5 +1,9 @@
 package com.ib.user.dto;
 
+import com.ib.util.validation.Password;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,5 +14,8 @@ import lombok.NoArgsConstructor;
 public class PasswordRotationDto {
 	private String userEmail;
 	private String oldPassword;
+	@NotNull
+	@Password
+	@Size(min=8, message="Must contain at least 8 characters")
 	private String newPassword;
 }
--- back/sssscs/src/main/java/com/ib/user/dto/UserCreateDto.java
+++ back/sssscs/src/main/java/com/ib/user/dto/UserCreateDto.java
@@ -4,6 +4,7 @@ import com.ib.util.validation.Password;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,19 +16,23 @@ import lombok.NoArgsConstructor;
 public class UserCreateDto {
 	@NotNull
 	@Email
-	@Size(max = 100)
+	@Size(min=2, max = 100, message="Must contain between 2 and 100 characters")
 	private String email;
 	@NotNull
 	@Password
+	@Size(min=8, message="Must contain at least 8 characters")
 	private String password;
 	@NotNull
-	@Size(max = 100)
+	@Size(min=2, max=30, message="Must contain between 2 and 30 characters")
+	@Pattern(regexp = "^[A-Za-z][A-Za-z ]*[A-Za-z]$", message="Must be letters or spaces (except at the start and end)")
 	private String name;
 	@NotNull
-	@Size(max = 100)
+	@Size(min=2, max=30, message="Must contain between 2 and 30 characters")
+	@Pattern(regexp = "^[A-Za-z][A-Za-z ]*[A-Za-z]$", message="Must be letters or spaces (except at the start and end)")
 	private String surname;
 	@NotNull
-	@Size(max = 18)
+	@Size(max = 18, message="Must contain at most 18 characters")
+	@Pattern(regexp = "^[0-9]*$", message="Must be numbers")
 	private String phoneNumber;
 	@NotNull
 	private String token;
