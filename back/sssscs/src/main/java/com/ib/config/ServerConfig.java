5e8207982a537695606d3bbc1ce3e8536e226861
magley <magazinlazar2@gmail.com>
2023-06-04 14:43:51 +0200

    feat: Add logback dependency

--- back/sssscs/src/main/java/com/ib/config/ServerConfig.java
+++ back/sssscs/src/main/java/com/ib/config/ServerConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
